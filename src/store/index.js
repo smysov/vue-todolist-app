d8f195d6d84fa6563eceaaee5dc96e44446b3f0d
Sergey Mysov <s.mysov@list.ru>
2021-03-14 21:50:22 +0400

    feat: added tasks to local storage

--- src/store/index.js
+++ src/store/index.js
@@ -50,9 +50,11 @@ export default new Vuex.Store({
   mutations: {
     ADD_TASK({ tasks }, payload) {
       tasks.push(payload);
+      localStorage.setItem('tasks', JSON.stringify(tasks));
     },
     DELETE_TASK(state, payload) {
       state.tasks = state.tasks.filter((task) => task.id !== payload);
+      localStorage.setItem('tasks', JSON.stringify(state.tasks));
     },
     CHANGE_CLASS_TASK(state, payload) {
       state.column = payload;
@@ -95,7 +97,7 @@ export default new Vuex.Store({
       return search;
     },
     getFilteredTasks({ tasks, search }) {
-      if (!search) return tasks;
+      if (!search) return JSON.parse(localStorage.getItem('tasks')) || tasks;
       const searchLowerCase = search.trim().toLocaleLowerCase();
       const filteredTasks = tasks.filter(
         (task) => task.title.toLocaleLowerCase().indexOf(searchLowerCase) !== -1,
